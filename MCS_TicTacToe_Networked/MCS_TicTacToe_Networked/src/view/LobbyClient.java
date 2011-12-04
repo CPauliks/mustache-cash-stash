4c1730120589306a6633f12339ff17269467675b
benLogN <sleeping123@gmail.com>
2011-12-04 03:45:20 -0600

    Removed some unused imports to kill some warnings.

--- MCS_TicTacToe_Networked/MCS_TicTacToe_Networked/src/view/LobbyClient.java
+++ MCS_TicTacToe_Networked/MCS_TicTacToe_Networked/src/view/LobbyClient.java
@@ -1,14 +1,11 @@
 //BEGIN FILE LobbyClient.java
 package view;
 
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpResponseException;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -17,8 +14,6 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
 
 import controller.User;
 
